2ac371a86692b16719fda90bc8f6a887dfc969f3
Brian Isganitis <brianji@google.com>
2020-04-06 21:43:10 -0400

    Add fish model.

---
+++ lib/fish.dart
@@ -0,0 +1,50 @@
+import 'package:intl/intl.dart';
+import 'package:meta/meta.dart';
+
+class Fish {
+  static final _monthFormat = DateFormat.MMM();
+  static final _timeFormat = DateFormat('ha');
+
+  final String name;
+  final String location;
+  final String size;
+  final int price;
+  final String time;
+  final String north;
+  final String south;
+
+  const Fish({
+    @required this.name,
+    @required this.location,
+    @required this.price,
+    @required this.time,
+    @required this.north,
+    @required this.south,
+    @required this.size,
+  });
+
+  bool get isAvailable {
+    final now = DateTime.now().toLocal();
+    final month = now.month;
+    final hour = now.hour;
+    final months = north.split(', ');
+    final times = time.split(', ');
+
+    return months.any((m) {
+          if (m == 'Any') return true;
+          final range = m.split('-');
+          final start = _monthFormat.parse(range.first).month;
+          final end = _monthFormat.parse(range.last).month;
+          return (start <= end && month >= start && month <= end) ||
+              (start > end && (month <= end || month >= start));
+        }) &&
+        times.any((t) {
+          if (t == 'Any') return true;
+          final range = t.split('-');
+          final start = _timeFormat.parse(range.first).hour;
+          final end = _timeFormat.parse(range.last).hour;
+          return (start <= end && hour >= start && hour < end) ||
+              (start > end && (hour < end || hour >= start));
+        });
+  }
+}
