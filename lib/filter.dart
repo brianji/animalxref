1cfb7d73138831a95cc4e05a9863b7ad8328fadc
Brian Isganitis <brianji@google.com>
2020-04-06 21:48:20 -0400

    Implement filter model.

---
+++ lib/filter.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/foundation.dart';
+
+enum Sort {
+  name,
+  bells,
+  size,
+}
+
+enum Location {
+  any,
+  river,
+  pond,
+  clifftop,
+  mouth,
+  sea,
+  pier,
+  rain,
+}
+
+const locationText = {
+  Location.any: 'Any',
+  Location.river: 'River',
+  Location.pond: 'Pond',
+  Location.clifftop: 'Clifftop',
+  Location.mouth: 'Mouth',
+  Location.sea: 'Sea',
+  Location.pier: 'Pier',
+  Location.rain: 'Rain',
+};
+
+enum Time {
+  any,
+  now,
+}
+
+enum FishSize {
+  any,
+  smallest,
+  small,
+  medium,
+  large,
+  xLarge,
+  largest,
+  fin,
+  narrow,
+}
+
+const fishSizeText = {
+  FishSize.any: 'Any',
+  FishSize.smallest: 'Smallest',
+  FishSize.small: 'Small',
+  FishSize.medium: 'Medium',
+  FishSize.large: 'Large',
+  FishSize.xLarge: 'X Large',
+  FishSize.largest: 'Largest',
+  FishSize.fin: 'Fin',
+  FishSize.narrow: 'Narrow',
+};
+
+const fishSizeIndex = {
+  'Any': 0,
+  'Smallest': 1,
+  'Small': 2,
+  'Medium': 3,
+  'Large': 4,
+  'X Large': 5,
+  'Largest': 6,
+  'Fin': 7,
+  'Narrow': 8,
+};
+
+enum Donate {
+  any,
+  no,
+}
+
+class FilterNotifier extends ChangeNotifier {
+  Location _location;
+  Sort _sort;
+  Time _time;
+  FishSize _fishSize;
+  Donate _donate;
+
+  FilterNotifier() {
+    _location = Location.any;
+    _sort = Sort.name;
+    _time = Time.any;
+    _fishSize = FishSize.any;
+    _donate = Donate.any;
+  }
+
+  set location(Location location) {
+    if (location == _location) return;
+    _location = location;
+    notifyListeners();
+  }
+
+  set sort(Sort sort) {
+    if (sort == _sort) return;
+    _sort = sort;
+    notifyListeners();
+  }
+
+  set time(Time time) {
+    if (time == _time) return;
+    _time = time;
+    notifyListeners();
+  }
+
+  set fishSize(FishSize fishSize) {
+    if (fishSize == _fishSize) return;
+    _fishSize = fishSize;
+    notifyListeners();
+  }
+
+  set donate(Donate donate) {
+    if (donate == _donate) return;
+    _donate = donate;
+    notifyListeners();
+  }
+
+  Location get location => _location;
+  Sort get sort => _sort;
+  Time get time => _time;
+  FishSize get fishSize => _fishSize;
+  Donate get donate => _donate;
+}
