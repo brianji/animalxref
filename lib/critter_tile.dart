ef8d460788fb747f7a43d8ecaae2595afe0e2dc1
Brian Isganitis <brianji03@gmail.com>
2020-10-10 23:00:52 -0400

    Make tiles dense.

--- lib/critter_tile.dart
+++ lib/critter_tile.dart
@@ -13,6 +13,7 @@ class CritterTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      dense: true,
       title: Text(critter.name),
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
